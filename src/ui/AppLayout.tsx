318ac83ae0410511af1c47fa2ac6a4d7dc0be311
Zouhair-Al-Yazji <alyazjyzhyr@gmail.com>
2025-07-26 18:55:34 +0300

    remove the transitions form the stayling

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -7,8 +7,8 @@ export default function AppLayout() {
     <div className="grid h-dvh grid-cols-[16.25rem_1fr] grid-rows-[auto_1fr]">
       <Header />
       <Sidebar />
-      <main className="bg-background overflow-auto px-12 pt-10 pb-16 transition-colors duration-300">
-        <div className="mx-auto my-0 flex max-w-[1200px] flex-col gap-8 transition-colors duration-300">
+      <main className="bg-background overflow-auto px-12 pt-10 pb-16">
+        <div className="mx-auto my-0 flex max-w-[1200px] flex-col gap-8">
           <Outlet />
         </div>
       </main>
